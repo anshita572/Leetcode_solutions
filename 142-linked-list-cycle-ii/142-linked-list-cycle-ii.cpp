82e4f3a4b0087c3d4863cf52bd1434e7290647fb
anshita572 <100852369+anshita572@users.noreply.github.com>
2022-04-12 12:11:43 +0530

    Time: 8 ms (81.33%), Space: 7.7 MB (34.51%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public: //Floyd detection
+    ListNode* hasCycle(ListNode *head) {
+        //empty LL or LL having 1 node
+        if(head==NULL || head->next==NULL)
+        {return NULL;}
+       //if we don't include case of LL having 1 node then slow==fast and it will return true
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(slow!=NULL && fast!=NULL)
+        { 
+         fast=fast->next;
+         if(fast!=NULL)
+         {fast=fast->next;}
+         slow=slow->next;
+         if(slow==fast)
+         {return fast;} //or return slow as fast and slow both at same postion
+        }
+        return NULL; //no cycle found 
+    }
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *fast=hasCycle(head);
+        ListNode*slow=head;
+        if(fast==NULL)
+        {return NULL;}
+        while(slow!=fast)
+        {
+            slow=slow->next;
+            fast=fast->next;
+        }
+        return slow;  //or return fast as both at same position
+    }
+};
