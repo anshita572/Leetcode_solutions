186ea572b3857e662661bb024505d78bb6a7f697
anshita572 <100852369+anshita572@users.noreply.github.com>
2022-04-28 22:16:11 +0530

    Time: 17 ms (86.29%), Space: 11 MB (55.40%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans="";
+        for(int i=0;i<s.size();i++)
+        {
+            if(ans.size()!=0)
+            {
+                if(ans[ans.size()-1]==s[i])
+                {
+                    ans.pop_back();
+                }
+                else
+                {
+                    ans.push_back(s[i]);
+                }
+            }
+            else
+            {
+                ans.push_back(s[i]);
+            }
+        }
+        return ans;
+    }
+};
