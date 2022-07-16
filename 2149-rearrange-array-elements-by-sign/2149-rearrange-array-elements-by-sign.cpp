8cb23c9d6a40d2a1e9ac23741f635258f3823b01
Anshita <100852369+anshita572@users.noreply.github.com>
2022-07-16 19:32:20 +0530

    Time: 234 ms (94.89%), Space: 134.5 MB (35.71%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,20 +1,45 @@
 class Solution {
 public:
-    vector<int> rearrangeArray(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>ans(n);
-        int positiveIndex=0;
-        int negativeIndex=1;
+    vector<int> rearrangeArray(vector<int>& arr) {
+        vector<int>positive,negative;
+        int n=arr.size();
+	    for(int i=0;i<n;i++)
+	    {
+	        if(arr[i]>=0)
+	        {positive.push_back(arr[i]);}
+	        else// if(arr[i]<0)
+	        {negative.push_back(arr[i]);}
+	    }
+	    //merge both subaarays into arr
+	    int i=0,j=0,k=0;
+	    while(i<n)
+	    {
+	       
+	        if(j<positive.size())
+	        {
+	            arr[i++]=positive[j++];
+	        }
+	          if(k<negative.size())
+	        {
+	            arr[i++]=negative[k++];
+	        }
+	        
+	    }
+        return arr;
+//         int n=nums.size();
+//         vector<int>ans(n);
+//         int positiveIndex=0;
+//         int negativeIndex=1;
         
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]>0)
-            {ans[positiveIndex]=nums[i];
-            positiveIndex=positiveIndex+2;}
-            else if(nums[i]<0)
-            {ans[negativeIndex]=nums[i];
-            negativeIndex=negativeIndex+2;}
-        }
-        return ans;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(nums[i]>0)
+//             {ans[positiveIndex]=nums[i];
+//             positiveIndex=positiveIndex+2;}
+//             else if(nums[i]<0)
+//             {ans[negativeIndex]=nums[i];
+//             negativeIndex=negativeIndex+2;}
+//         }
+//         return ans;
     }
 };
