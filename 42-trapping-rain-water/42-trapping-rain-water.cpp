5920ac662ad119036b1dabf08521ba43c2d5b1f9
Anshita <100852369+anshita572@users.noreply.github.com>
2022-07-25 17:13:27 +0530

    Time: 20 ms (70.98%), Space: 19.8 MB (75.82%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -2,24 +2,31 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n=height.size();
-        vector<int>arr1(n);
-        vector<int>arr2(n);
-        int prefix_sum=0;
-        int suffix_sum=0;
         int ans=0;
-        for(int i=0;i<n;i++)
+        int l=0;
+        int r=n-1;
+        int left_max=0;
+        int right_max=0;
+        
+       while(l<=r)
         {
-            prefix_sum=max(height[i],prefix_sum);
-            arr1[i]=prefix_sum;
-        }
-         for(int j=n-1;j>=0;j--)
-        {
-            suffix_sum=max(height[j],suffix_sum);
-             arr2[j]=suffix_sum;
-        }
-        for(int k=0;k<n;k++)
-        {
-            ans=ans+(min(arr1[k],arr2[k])-height[k]);
+            if(height[l]<=height[r])
+            {
+                if(height[l]>=left_max)
+                {left_max=height[l];}
+                else //if(height[l]<left_max)
+                {ans=ans+(left_max-height[l]);}
+                l++;
+            }
+            else //if(height[l]>height[r])
+            {
+                if(height[r]>=right_max)
+                {right_max=height[r];}
+                else //if(height[r]<right_max)
+                {ans=ans+(right_max-height[r]);}
+                r--;
+                    
+            }
         }
         return ans;
     }
