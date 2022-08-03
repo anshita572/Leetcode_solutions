71e0f7f2976931f661981c48dc43e2958ad85e99
Anshita <100852369+anshita572@users.noreply.github.com>
2022-08-03 19:14:57 +0530

    Time: 266 ms (67.14%), Space: 75.1 MB (95.24%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int mini=INT_MAX;
+        int mini1=INT_MAX;
         int mini2=INT_MAX;
-        int profit=0; //overall profit
-         int profit2=0;
-        int temp_profit=0; //profit if sold now
+        int profit1=0; 
+        int profit2=0;
+        int temp_profit1=0; 
+        int temp_profit2=0;
+        int netBuyPrice=0;
         int n=prices.size();
         for(int i=0;i<n;i++)
         {
-            mini=min(prices[i],mini);
-            temp_profit=prices[i]-mini;
-            if(temp_profit > profit)
-            {profit=temp_profit;}
-            mini2=min(prices[i]-profit,mini2);
-                profit2=max(profit2,prices[i]-mini2);
+            mini1=min(prices[i],mini1);
+            temp_profit1=prices[i]-mini1;
+            if(temp_profit1 > profit1)
+            {profit1=temp_profit1;}
+            netBuyPrice=prices[i]-profit1;
+            mini2=min(netBuyPrice,mini2);
+            temp_profit2=prices[i]-mini2;
+             if(temp_profit2 > profit2)
+            {profit2=temp_profit2;}
         }
         return profit2;
     }
