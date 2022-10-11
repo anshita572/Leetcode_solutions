a76ec3d0e1519217c744c2ce03a1581d6ed5a823
Anshita <100852369+anshita572@users.noreply.github.com>
2022-10-12 00:03:05 +0530

    Time: 272 ms (5.01%), Space: 9.7 MB (78.30%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -1,35 +1,11 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-        int len=arr.size();
-        int count=0;
-        for(int i=0;i<len;i++)
+        for(int i=arr.size()-1;i>=0;i--)
         {
             if(arr[i]==0)
-            {count++;}
-        }
-        int new_len=len+count;
-        for(int i=len-1,j=new_len-1;i>=0 && j>=0;i--,j--)
-        {
-            // if(j<len)
-            // {arr[j]=arr[i];
-            // if(arr[i]==0)
-            // {j--;
-            // arr[j]=arr[i];}
-            // }
-            if(arr[i]!=0)
-            {
-                if(j<len)
-                {arr[j]=arr[i];}
-                
-            }
-            else
-            {
-                if(j<len)
-                {arr[j]=arr[i];}
-                j--;
-                if(j<len)
-                {arr[j]=arr[i];}
+            {for(int j=arr.size()-1;j>i;j--)
+            {arr[j]=arr[j-1];}
             }
         }
     }
