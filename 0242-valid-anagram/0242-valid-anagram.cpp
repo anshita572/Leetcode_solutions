5e347ff0b300d2c1c58735c89140470af1b2b023
Anshita <100852369+anshita572@users.noreply.github.com>
2022-10-28 19:31:35 +0530

    Time: 8 ms (90.61%), Space: 7.4 MB (56.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t)
-        {return true;}
-        else
-        {return false;}
+       if(s.size()!=t.size())
+       {return false;}
+        int arr[26]={0};
+        for(int i=0;i<s.size();i++)
+        {arr[s[i]-'a']++;
+        arr[t[i]-'a']--;}
+        for(int i=0;i<26;i++)
+        {if(arr[i] !=0)
+        {return false;}}
+        return true;
     }
 };
