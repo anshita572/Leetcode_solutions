e87a620ff9ef32cddaa73b0b5b6ae2b52f8ff1ca
Anshita <100852369+anshita572@users.noreply.github.com>
2022-11-02 10:52:52 +0530

    Time: 2044 ms (11.74%), Space: 76.8 MB (91.28%) - LeetHub

--- 1655-distribute-repeating-integers/1655-distribute-repeating-integers.cpp
+++ 1655-distribute-repeating-integers/1655-distribute-repeating-integers.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     bool solve( vector<int>& quantity, unordered_map<int,int>&mp,int index)
-    {if(index==quantity.size())
+    {//base case
+        if(index==quantity.size())
     {return true;}
     
-    for(auto i=mp.begin(); i!=mp.end();i++)
+    for(auto i:mp)
     {
-        if(i->second>=quantity[index])
-        {   mp[i->first]=mp[i->first]-quantity[index];
-            bool recursiveAns=solve(quantity,mp,index+1);
+        if(i.second>=quantity[index])
+        {   mp[i.first]=mp[i.first]-quantity[index];
+            bool recursiveAns=solve(quantity,mp,index+1);//recursive call
         if(recursiveAns)
         {return true;}
         
-        mp[i->first]=mp[i->first]+quantity[index];
+        mp[i.first]=mp[i.first]+quantity[index];//backtracking
         }
     }
     return false;
 }
     bool canDistribute(vector<int>& nums, vector<int>& quantity) {
          unordered_map<int,int>mp;
+        //Store frequency of nums vector in map
         for(int i=0;i<nums.size();i++)
         {mp[nums[i]]++;}
         int index=0;
+        //sort quantity vector
         sort(quantity.begin(),quantity.end(),greater<int>());
         bool ans= solve(quantity,mp,index);
         return ans;
