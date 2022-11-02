44804f5a48d0513ccdceaecb65fa91713ac4b538
Anshita <100852369+anshita572@users.noreply.github.com>
2022-11-02 19:32:30 +0530

    Time: 4 ms (37.41%), Space: 7.4 MB (26.26%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void solve(string s,int index,unordered_set<string>st,string temp,  vector<string>&ans)
+    {
+        //base case
+        if(index==s.length())
+        {temp.pop_back();
+        ans.push_back(temp);
+        return;}
+        string subString="";
+        for(int i=index;i<s.length();i++)
+        {
+            subString.push_back(s[i]);
+            if(st.count(subString))
+            {solve(s,i+1,st,temp+subString+" ",ans);}
+        }
+    }
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string>st;
+        for(auto it:wordDict)
+        {st.insert(it);}
+        string temp="";
+         vector<string>ans;
+        solve(s,0,st,temp,ans);
+        return ans;
+    }
+};
