cc3876b3e5329bf5c2c70812b351d921a1e10365
Anshita <100852369+anshita572@users.noreply.github.com>
2022-11-03 19:23:38 +0530

    Time: 1784 ms (15.66%), Space: 7.5 MB (85.85%) - LeetHub

--- 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.cpp
+++ 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.cpp
@@ -13,15 +13,15 @@ public:
         {if(j>0 && work[j]==work[j-1]) //optimisation
         {continue;}
             work[j]=work[j]+jobs[i];
-        solve(jobs,work,n,k,i+1,work_array_max,ans);
-        work[j]=work[j]-jobs[i];}
+        solve(jobs,work,n,k,i+1,work_array_max,ans);//recursive call
+        work[j]=work[j]-jobs[i];}//backtracking
         
     }
     int minimumTimeRequired(vector<int>& jobs, int k) {
-        sort(jobs.begin(),jobs.end(),greater<int>());
+        // sort(jobs.begin(),jobs.end(),greater<int>());
         int n=jobs.size();
-        if(n==k)
-        {return jobs[0];}//max element in jobs array after sorting in descending order
+        // if(n==k)
+        // {return jobs[0];}//max element in jobs array after sorting in descending order
         vector<int>work(k);
         int work_array_max=0;
         int ans=INT_MAX;
