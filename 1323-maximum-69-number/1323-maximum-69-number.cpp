6e8eccd2b4d15b9d4088c4c6b35ae899bccb0db3
Anshita <100852369+anshita572@users.noreply.github.com>
2022-11-08 00:36:57 +0530

    Time: 3 ms (38.22%), Space: 6.1 MB (8.65%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        string n=to_string(num);
-        for(int i=0;i<n.size();i++)
-        {if(n[i]=='6')
-        {n[i]='9';
-        break;}
-        }
-        int n1=stoi(n);
-        return n1;
+        int temp_num=num;
+        int digCount=0;
+        int right_digCount=-1;
+        while(temp_num>0)
+        {int last_dig=temp_num%10;
+        if(last_dig==6)
+        {right_digCount=digCount;}
+        digCount++;
+        temp_num=temp_num/10;}
+        // if(right_digCount==-1)
+        // {return num;}
+        int ans=num + 3*(pow(10,right_digCount));
+         return ans;
     }
+  
 };
