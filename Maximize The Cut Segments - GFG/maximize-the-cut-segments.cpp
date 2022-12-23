6d8646bdf56eed547a402fcee59df429ce2f557d
Anshita <100852369+anshita572@users.noreply.github.com>
2022-12-23 22:55:47 +0530

    Added solution - LeetHub

--- Maximize The Cut Segments - GFG/maximize-the-cut-segments.cpp
+++ Maximize The Cut Segments - GFG/maximize-the-cut-segments.cpp
@@ -9,18 +9,32 @@ using namespace std;
 class Solution
 {
     public:
-    int solve(int n,int x,int y,int z,vector<int>&dp)
+    int solveTab(int n,int x,int y,int z)
+    {vector<int>dp(n+1,INT_MIN);
+    dp[0]=0;
+    for(int i=1;i<=n;i++)
+    {if(i-x>=0)
+        {dp[i]=max(dp[i],dp[i-x]+1);}
+        if(i-y>=0)
+        {dp[i]=max(dp[i],dp[i-y]+1);}
+        if(i-z>=0)
+        {dp[i]=max(dp[i],dp[i-z]+1);}
+    }
+    int ans=dp[n];
+    return ans;
+    }
+    int solveMemo(int n,int x,int y,int z,vector<int>&dp)
     {if(n==0)
     {return 0;}
     if(n<0)
     {return INT_MIN;}
     if(dp[n]!=-1)
     {return dp[n];}
-    int op1=solve(n-x,x,y,z,dp);
+    int op1=solveMemo(n-x,x,y,z,dp);
      
-    int op2=solve(n-y,x,y,z,dp);
+    int op2=solveMemo(n-y,x,y,z,dp);
     
-    int op3=solve(n-z,x,y,z,dp);
+    int op3=solveMemo(n-z,x,y,z,dp);
     int ans=1+max(op1,max(op2,op3));
     dp[n]=ans;
     return ans;
@@ -30,9 +44,10 @@ class Solution
     //Function to find the maximum number of cuts.
     int maximizeTheCuts(int n, int x, int y, int z)
     {vector<int>dp(n+1,-1);
-        int ans= solve(n,x,y,z,dp);
+        // int ans= solveMemo(n,x,y,z,dp);
+        int ans= solveTab(n,x,y,z);
         if(ans<0)
-        {ans=0;}
+        {ans=0;} //if not possible ,return 0 according to question
         return ans;
     }
 };
