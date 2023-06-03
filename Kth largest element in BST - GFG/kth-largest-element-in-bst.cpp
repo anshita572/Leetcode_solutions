bcdd419b5009af3c8deb9fea8a46346e6ead2608
Anshita <100852369+anshita572@users.noreply.github.com>
2023-06-03 11:20:16 +0530

    Added solution - LeetHub

--- Kth largest element in BST - GFG/kth-largest-element-in-bst.cpp
+++ Kth largest element in BST - GFG/kth-largest-element-in-bst.cpp
@@ -97,19 +97,20 @@ struct Node {
 class Solution
 {
     public:
-    void inorder(Node* root,vector<int>&ans)
-    {if(root==NULL)
-    {return;}
-     inorder(root->left,ans);
-     ans.push_back(root->data);
-     inorder(root->right,ans);
+    int solve(Node*root,int &K)
+    {if(root == NULL)
+        {return -1;}
+        int r=solve(root->right,K);
+        if(r!=-1)
+        {return r;}
+        K--;
+        if(K==0)
+        {return root->data;}
+        return solve(root->left,K);
     }
     int kthLargest(Node *root, int K)
-    {vector<int>ans;
-       inorder(root,ans); 
-       int n=ans.size();
-       return ans[n-K];
-       
+    {
+        return solve(root,K);
     }
 };
 
