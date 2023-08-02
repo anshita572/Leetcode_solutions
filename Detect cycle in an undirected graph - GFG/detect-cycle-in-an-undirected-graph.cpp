2e5917ab0fac2e2332afa78d5fb1aa95e491c32d
Anshita <100852369+anshita572@users.noreply.github.com>
2023-08-02 10:47:51 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -6,37 +6,25 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
-    bool detect(int node, int vis[],vector<int> adj[]){
+    bool detect(int node,int parent ,int vis[],vector<int> adj[]){
         vis[node] = 1;
-        queue<pair<int,int>>q;
-        q.push({node,-1});
-        while(!q.empty()){
-            int front = q.front().first;
-            int parent = q.front().second;
-            q.pop();
-            for(auto i : adj[front]){
-                if(!vis[i]){
-                    vis[i] = 1;
-                    q.push({i,front});
-                }
-                else if (parent != i)  // else if it is visited but 
-                //it is not a parent
-                {return true;} //cycle found
-                //else if it is a parent and it is visted that's totally fine
-               //it does not mean that it has a cycle.Only parent is allowed
-              // to be visited, any other node that is not a parent but is
-              // already visited => there is a cycle
+        for(auto i : adj[node])
+        { if(!vis[i])
+            {if(detect(i,node,vis,adj)==true)
+                {return true;}
             }
+            else if (parent != i)
+            {return true;}
         }
         return false;
     }
     bool isCycle(int V, vector<int> adj[]) {
         int vis[V]={0};
-        
+        int parent = -1;
         // if there are multiple components
         for (int i=0;i<V;i++){
             if(!vis[i])
-            {if(detect(i,vis,adj))
+            {if(detect(i,parent,vis,adj))
                 {return true;}
             }
         }
