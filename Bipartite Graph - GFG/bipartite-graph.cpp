00f9f0955ac5c1411c80abf7aa5999986d1aaa48
Anshita <100852369+anshita572@users.noreply.github.com>
2023-08-03 19:35:08 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -4,40 +4,33 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-private: 
-    bool dfs(int node, int col, int color[], vector<int> adj[]) {
-        color[node] = col; 
-        
-        // traverse adjacent nodes
-        for(auto it : adj[node]) {
-            // if uncoloured
-            if(color[it] == -1) {
-                if(dfs(it, !col, color, adj) == false) return false; 
-            }
-            // if previously coloured and have the same colour
-            else if(color[it] == col) {
-                return false; 
-            }
-        }
-        
-        return true; 
-    }
 public:
+    bool dfs(int node, vector<int>adj[],int color[],int curr_color){
+        color[node] = curr_color;
+        for(auto i : adj[node]){
+            if(color[i] == -1){
+               if (dfs(i,adj,color,!curr_color)==false)
+               {return false;}
+            }
+            else if(color[i] == curr_color)
+            {return false;}
+        }
+        return true;
+    }
 	bool isBipartite(int V, vector<int>adj[]){
-	    int color[V];
-	    for(int i = 0;i<V;i++) color[i] = -1; 
-	    
-	    // for connected components
-	    for(int i = 0;i<V;i++) {
-	        if(color[i] == -1) {
-	            if(dfs(i, 0, color, adj) == false) 
-	                return false; 
+	   // vector<int>color (V,-1);
+	   int color[V];
+	   for(int i=0;i<V;i++)
+	    {color[i]=-1;}
+	    int curr_color = 0;
+	    for(int i=0;i<V;i++){
+	        if(color[i]==-1){
+	          if(dfs(i,adj,color,curr_color) == false)
+	            {return false;}
 	        }
 	    }
-	    return true; 
+	    return true;
 	}
-
-    
 
 };
 
