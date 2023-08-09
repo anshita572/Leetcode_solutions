b840715f2372383c86dd9b4ff0c6ad9d34ecb25f
Anshita <100852369+anshita572@users.noreply.github.com>
2023-08-09 09:44:13 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -10,27 +10,28 @@ class Solution
     //from the source vertex S.
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
-        priority_queue<pair<int,int> , vector<pair<int,int>> , 
-                        greater<pair<int,int>>>pq; //min heap
-       vector<int>distance(V);
-        for(int i = 0; i < V; i++){
-            distance[i] = INT_MAX;
-        }
+        set<pair<int,int>>st;
+        vector<int>distance(V,1e9);
         distance[S] = 0;
-        pq.push({S,0}); //pq.push({weight,node})
-        
-        while(!pq.empty()){
-            int dist = pq.top().second;
-            int node = pq.top().first;
-            pq.pop();
-            for(auto it : adj[node]){
-                int edgeWeight = it[1];
-                int adjNode = it[0];
+        st.insert({0,S});
+        while(!st.empty()){
+            auto it = *(st.begin());
+            int dist = it.first;
+            int node = it.second;
+            st.erase(it);   // just like we pop in queue
+            for(auto i : adj[node]){
+                int edgeWeight = i[1];
+                int adjNode = i[0];
                 if(dist + edgeWeight < distance[adjNode]){
+                    if(distance[adjNode] != 1e9){
+                        st.erase({distance[adjNode], adjNode});
+                    }
                     distance[adjNode] = dist + edgeWeight;
-                    pq.push({adjNode, distance[adjNode]});
+                    st.insert({distance[adjNode], adjNode});
+                    
                 }
             }
+            
         }
         return distance;
     }
